8b03761541971e189f9cb8d465ffb3bfb28b6e0b
Patt <pattaraphon.dch@gmail.com>
2026-05-09 16:38:57 +0700

    Update favicon

--- app/layout.tsx
+++ app/layout.tsx
@@ -16,6 +16,9 @@ const inter = Inter({
 export const metadata: Metadata = {
   title: 'Ramen House',
   description: 'Authentic Japanese ramen in the heart of the city',
+  icons: {
+    icon: "data:image/svg+xml,<svg xmlns='http://www.w3.org/2000/svg' viewBox='0 0 100 100'><text y='.9em' font-size='90'>🍜</text></svg>",
+  },
 }
 
 export default function RootLayout({
